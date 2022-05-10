b5cc02d672d507d5fd304a76599a76a1e5bea4f0
Tii <mail@tii.one>
2022-05-10 23:53:42 +0200

    Fixed InputMessageContent

--- src/Telegram/InputMessageContent.php
+++ src/Telegram/InputMessageContent.php
@@ -6,18 +6,11 @@
 
 namespace Tii\Telepath\Telegram;
 
-use Tii\Telepath\Types\Factory;
 use Tii\Telepath\Types\Type;
 
 /**
  * This object represents the content of a message to be sent as a result of an inline query. Telegram clients currently support the following 5 types:
  */
-abstract class InputMessageContent extends Type implements Factory
+abstract class InputMessageContent extends Type
 {
-    public static function factory(array $data): self
-    {
-        return match($data[null]) {
-            '' => new InputInvoiceMessageContent($data),
-        };
-    }
 }
