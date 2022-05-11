e1b204290bcd47f7cb584197af64ab5c018af792
Tii <mail@tii.one>
2022-05-11 11:16:00 +0200

    Accept InputFile for deeper attachments

--- src/Telegram/InputMediaAudio.php
+++ src/Telegram/InputMediaAudio.php
@@ -27,7 +27,7 @@ class InputMediaAudio extends InputMedia
     public ?string $title = null;
 
     /**
-     * @param string $media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. More info on Sending Files »
+     * @param string|\Tii\Telepath\Types\InputFile $media File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. More info on Sending Files »
      * @param \Tii\Telepath\Types\InputFile|string $thumb Optional. Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>. More info on Sending Files »
      * @param string $caption Optional. Caption of the audio to be sent, 0-1024 characters after entities parsing
      * @param string $parse_mode Optional. Mode for parsing entities in the audio caption. See formatting options for more details.
@@ -37,7 +37,7 @@ class InputMediaAudio extends InputMedia
      * @param string $title Optional. Title of the audio
      */
     public static function make(
-        string $media,
+        string|\Tii\Telepath\Types\InputFile $media,
         \Tii\Telepath\Types\InputFile|string|null $thumb = null,
         ?string $caption = null,
         ?string $parse_mode = null,
