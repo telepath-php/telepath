b3c7e16d1e01737b2130fca0b573709a7445145a
Tii <mail@tii.one>
2022-05-29 21:17:46 +0200

    Fixed non-optional fixed values

--- src/Telegram/ForceReply.php
+++ src/Telegram/ForceReply.php
@@ -14,7 +14,7 @@ use Tii\Telepath\Types\Type;
 class ForceReply extends Type
 {
     /** Shows reply interface to the user, as if they manually selected the bot's message and tapped 'Reply' */
-    public bool $force_reply;
+    public bool $force_reply = true;
 
     /** Optional. The placeholder to be shown in the input field when the reply is active; 1-64 characters */
     public ?string $input_field_placeholder = null;
@@ -23,17 +23,12 @@ class ForceReply extends Type
     public ?bool $selective = null;
 
     /**
-     * @param bool $force_reply Shows reply interface to the user, as if they manually selected the bot's message and tapped 'Reply'
      * @param string $input_field_placeholder Optional. The placeholder to be shown in the input field when the reply is active; 1-64 characters
      * @param bool $selective Optional. Use this parameter if you want to force reply from specific users only. Targets: 1) users that are @mentioned in the text of the Message object; 2) if the bot's message is a reply (has reply_to_message_id), sender of the original message.
      */
-    public static function make(
-        bool $force_reply,
-        ?string $input_field_placeholder = null,
-        ?bool $selective = null
-    ): static {
+    public static function make(?string $input_field_placeholder = null, ?bool $selective = null): static
+    {
         return new static([
-            'force_reply' => $force_reply,
             'input_field_placeholder' => $input_field_placeholder,
             'selective' => $selective,
         ]);
