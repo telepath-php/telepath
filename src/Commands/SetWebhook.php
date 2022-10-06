f7de03cd7f97dedb7352df3ab66574b661a55f4b
Tii <mail@tii.one>
2022-10-06 20:17:55 +0200

    Completed first console command

--- src/Commands/SetWebhook.php
+++ src/Commands/SetWebhook.php
@@ -4,59 +4,76 @@ namespace Telepath\Commands;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\Question;
-use Telepath\TelegramBot;
+use Telepath\Exceptions\TelegramException;
+use Telepath\Types\InputFile;
+use function Termwind\{render};
 
 #[AsCommand(
     name: 'set-webhook',
     description: 'Registers a webhook with Telegram servers.'
 )]
-class SetWebhook extends Command
+class SetWebhook extends BotCommand
 {
 
     protected function configure()
     {
-        $this->addOption('api-url', null, InputOption::VALUE_OPTIONAL, 'URL to the Bot API Server to use', false);
-        $this->addOption('token', null, InputOption::VALUE_REQUIRED, 'Telegram Bot API Token');
+        $this->configureBotArguments();
 
-        $this->addArgument('url', InputArgument::OPTIONAL, 'URL that points to your web endpoint');
+        $this->addArgument('url', InputArgument::OPTIONAL, 'HTTPS URL to send updates to');
+
+        $this->addOption('certificate', 'c', InputOption::VALUE_REQUIRED, 'Path to your public key certificate');
+        $this->addOption('ip-address', 'i', InputOption::VALUE_REQUIRED,
+            'The fixed IP address which will be used to send webhook requests instead of the IP address resolved through DNS');
+        $this->addOption('max-connections', 'm', InputOption::VALUE_REQUIRED,
+            'The maximum allowed number of simultaneous HTTPS connections to the webhook for update delivery');
+        $this->addOption('allowed-updates', 'a', InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+            'A list of the update types you want your bot to receive. For example, specify --allowed-updates message --allowed-updates edited_channel_post --allowed-updates callback_query');
+        $this->addOption('drop-pending-updates', 'd', InputOption::VALUE_NONE, 'Drop all pending updates');
+        $this->addOption('secret-token', 's', InputOption::VALUE_REQUIRED,
+            'A secret token to be sent in a header “X-Telegram-Bot-Api-Secret-Token” in every webhook request');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $apiUrl = match($input->getOption('api-url')) {
-            null => $this->ask($input, $output,
-                '<question>URL to Bot API Server to use</question>: '),
-            false => TelegramBot::DEFAULT_API_SERVER_URL,
-            default => $input->getOption('api-url')
-        };
+        parent::execute($input, $output);
 
-        $output->writeln("Got for API URL: $apiUrl");
+        $bot = $this->makeBot($input, $output);
 
+        $url = $input->getArgument('url') ?? $this->ask('Webhook URL: ');
+        $certificate = $input->getOption('certificate');
+        $ipAddress = $input->getOption('ip-address');
+        $maxConnections = $input->getOption('max-connections');
+        $allowedUpdates = $input->getOption('allowed-updates') ?: null;
+        $dropPendingUpdates = $input->getOption('drop-pending-updates') ?: null;
+        $secretToken = $input->getOption('secret-token');
+
+        $certificateFile = $certificate !== null ? InputFile::fromFile($certificate) : null;
+
+        try {
+            $bot->setWebhook(
+                url: $url,
+                certificate: $certificateFile,
+                ip_address: $ipAddress,
+                max_connections: $maxConnections,
+                allowed_updates: $allowedUpdates,
+                drop_pending_updates: $dropPendingUpdates,
+                secret_token: $secretToken
+            );
+        } catch (TelegramException $e) {
+            render(<<<HTML
+                <div class="error">{$e->getMessage()}</div>
+            HTML);
+            return Command::FAILURE;
+        }
+
+        render(<<<'HTML'
+            <div class="success">Webhook was set successfully.</div>
+        HTML);
         return Command::SUCCESS;
-    }
-
-    protected function ask(InputInterface $input, OutputInterface $output, string $question, $default = null): string
-    {
-        /** @var QuestionHelper $helper */
-        $helper = $this->getHelper('question');
-
-        $question = new Question($question, $default);
-        $question->setValidator(function ($answer) {
-            if ($answer === null) {
-                throw new \RuntimeException('You need to pass a value.');
-            }
-
-            return $answer;
-        });
-
-        return $helper->ask($input, $output, $question);
     }
 
 }
