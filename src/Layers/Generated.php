cd347e019584048baa02c92a8fcf554431fefd23
TiiFuchs <TiiFuchs@users.noreply.github.com>
2023-02-25 07:30:46 +0000

    Update code to reflect latest changes to the API documentation

--- src/Layers/Generated.php
+++ src/Layers/Generated.php
@@ -1393,7 +1393,7 @@ abstract class Generated extends Base
     }
 
     /**
-     * Use this method to change the default administrator rights requested by the bot when it's added as an administrator to groups or channels. These rights will be suggested to users, but they are are free to modify the list before adding the bot. Returns True on success.
+     * Use this method to change the default administrator rights requested by the bot when it's added as an administrator to groups or channels. These rights will be suggested to users, but they are free to modify the list before adding the bot. Returns True on success.
      *
      * @param ChatAdministratorRights $rights A JSON-serialized object describing new default administrator rights. If not specified, the default administrator rights will be cleared.
      * @param bool $for_channels Pass True to change the default administrator rights of the bot in channels. Otherwise, the default administrator rights of the bot for groups and supergroups will be changed.
