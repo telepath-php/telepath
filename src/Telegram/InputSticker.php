68697c252b54f5e2522ae4c7c580e8e79ae01924
TiiFuchs <TiiFuchs@users.noreply.github.com>
2023-03-19 23:22:44 +0000

    Update code to reflect latest changes to the API documentation

--- src/Telegram/InputSticker.php
+++ src/Telegram/InputSticker.php
@@ -13,7 +13,7 @@ use Telepath\Types\Type;
  */
 class InputSticker extends Type
 {
-    /** The added sticker. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. Animated and video stickers can't be uploaded via HTTP URL. More information on Sending Files » */
+    /** The added sticker. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, upload a new one using multipart/form-data, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. Animated and video stickers can't be uploaded via HTTP URL. More information on Sending Files » */
     public \Telepath\Types\InputFile|string $sticker;
 
     /**
@@ -32,7 +32,7 @@ class InputSticker extends Type
     public ?array $keywords = null;
 
     /**
-     * @param \Telepath\Types\InputFile|string $sticker The added sticker. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, or upload a new one using multipart/form-data. Animated and video stickers can't be uploaded via HTTP URL. More information on Sending Files »
+     * @param \Telepath\Types\InputFile|string $sticker The added sticker. Pass a file_id as a String to send a file that already exists on the Telegram servers, pass an HTTP URL as a String for Telegram to get a file from the Internet, upload a new one using multipart/form-data, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. Animated and video stickers can't be uploaded via HTTP URL. More information on Sending Files »
      * @param string[] $emoji_list List of 1-20 emoji associated with the sticker
      * @param MaskPosition $mask_position Optional. Position where the mask should be placed on faces. For “mask” stickers only.
      * @param string[] $keywords Optional. List of 0-20 search keywords for the sticker with total length of up to 64 characters. For “regular” and “custom_emoji” stickers only.
