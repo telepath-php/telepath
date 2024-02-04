4f0889a6c2287adfd5379b8d70fbbeea173947c8
Tii <mail@tii.one>
2024-02-04 17:35:24 +0100

    Correct behaviour on InputMessageContent::factory (non existent)

--- src/Telegram/InputMessageContent.php
+++ src/Telegram/InputMessageContent.php
@@ -6,23 +6,11 @@
 
 namespace Telepath\Telegram;
 
-use Telepath\Bot;
-use Telepath\Types\Factory;
 use Telepath\Types\Type;
 
 /**
  * This object represents the content of a message to be sent as a result of an inline query. Telegram clients currently support the following 5 types:
  */
-abstract class InputMessageContent extends Type implements Factory
+abstract class InputMessageContent extends Type
 {
-    public static function factory(array $data, Bot $bot = null): self
-    {
-        return match ($data[null]) {
-            null => new InputTextMessageContent($data, $bot),
-            null => new InputLocationMessageContent($data, $bot),
-            null => new InputVenueMessageContent($data, $bot),
-            null => new InputContactMessageContent($data, $bot),
-            null => new InputInvoiceMessageContent($data, $bot),
-        };
-    }
 }
