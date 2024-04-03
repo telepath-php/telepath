89ac02dd126a31688b5fa3259400ff350d52eade
TiiFuchs <1958744+TiiFuchs@users.noreply.github.com>
2024-04-03 23:32:43 +0000

    Update code to reflect latest changes to the Bot API documentation

--- src/Telegram/BusinessOpeningHoursInterval.php
+++ src/Telegram/BusinessOpeningHoursInterval.php
@@ -10,15 +10,15 @@ use Telepath\Types\Type;
 
 class BusinessOpeningHoursInterval extends Type
 {
-    /** The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 <em> 24 </em> 60 */
+    /** The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 * 24 * 60 */
     public int $opening_minute;
 
-    /** The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 <em> 24 </em> 60 */
+    /** The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 * 24 * 60 */
     public int $closing_minute;
 
     /**
-     * @param  int  $opening_minute  The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 <em> 24 </em> 60
-     * @param  int  $closing_minute  The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 <em> 24 </em> 60
+     * @param  int  $opening_minute  The minute's sequence number in a week, starting on Monday, marking the start of the time interval during which the business is open; 0 - 7 * 24 * 60
+     * @param  int  $closing_minute  The minute's sequence number in a week, starting on Monday, marking the end of the time interval during which the business is open; 0 - 8 * 24 * 60
      */
     public static function make(int $opening_minute, int $closing_minute): static
     {
